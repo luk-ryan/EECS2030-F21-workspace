765df96d9ef4a61d870b8b06cdf217c7cfaf3dc2
luk-ryan <ryankluk@my.yorku.ca>
2021-09-08 19:05:03 -0400

    Lab0 - EECS2030 F21
    worked on a little bit of lab0 and set everything up

---
+++ AppleShop/src/model/Product.java
@@ -0,0 +1,20 @@
+package model;
+
+/*
+ * Template for individual apple product in refurbished store.
+ */
+public class Product {
+	/* attributes (private means they are only visible within current class) */
+	private String model; // e.g.iPad Pro 12.9
+	private String finish; // e.g. silver, space grey
+	private int storage; // in the units of GB
+	private boolean hasCellularConnectivity; // e.g. false(only wifi), true(wifi + cellular data)
+	private double originalPrice; // e.g. 1789.00
+	private double discountValue; // e.g. 200.00
+	
+	/* constructors */
+	
+	/* accessors */
+	
+	/* mutators */
+}
